6b8eff5f387d0a42fc315a99d65f01462fc92ef7
lazy_ycp <1195800097@qq.com>
2023-09-16 00:12:02 +0800

    0:11

--- src/views/Images/Image.vue
+++ src/views/Images/Image.vue
@@ -1,27 +1,36 @@
 <template>
-  <div id="images">
+  <div id="images" data-app>
     <div class="table-wrapper">
     <h2>镜像管理</h2>
     <div class="card-style">
     <v-card>
     <v-card-title>
     <span>镜像列表</span>
-        <!-- 新镜像 -->
+        </v-card-title>
+    <v-card-title>
+      <v-text-field
+        v-model="search"
+        append-icon="mdi-magnify"
+        label="Search"
+        single-line
+        hide-details
+      ></v-text-field>
+       <v-row justify="center">
     <v-dialog
       v-model="dialog"
       persistent
       max-width="600px"
     >
-    <template v-slot:activator="{ on, attrs }">
-    <v-btn
-      depressed
-      color="primary"
-      style="margin:auto;float:right"
-      v-bind="attrs"
-      v-on="on"
-    >
-      创建新镜像
-    </v-btn>
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+          color="primary"
+          v-bind="attrs"
+          v-on="on"
+          dark
+          style="margin-left:100px"
+        >
+          创建镜像
+        </v-btn>
       </template>
       <v-card>
         <v-card-title>
@@ -118,16 +127,7 @@
         </v-card-actions>
       </v-card>
     </v-dialog>
-        </v-card-title>
-    <v-card-title>
-      <v-text-field
-        v-model="search"
-        append-icon="mdi-magnify"
-        label="Search"
-        single-line
-        hide-details
-      ></v-text-field>
-
+  </v-row>
     </v-card-title>
     <v-data-table
       :headers="headers"
@@ -178,8 +178,15 @@
       }
     },
     methods: {
-      newImage(){
-
+      addImage(){
+        this.show=true;
+      },
+      cancer(){
+        this.show=false;
+      },
+      submitImage(){
+        this.show=false;
+        //提交表单
       }
     },
   }
